983a01e573dc7398bd44663fc5842949d0a8bde0
Jaspreet Singh <123469919+Jass6268@users.noreply.github.com>
2024-01-15 15:26:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -251,7 +251,7 @@ async def base_site_handler(bot, m:Message):
     user = await get_user(user_id)
     cmd = m.command
     site = user['base_site']
-    text = f"`/base_site (base_site)`\n\nCurrent base site: {site}\n\n EX: `/base_site shareus.in`\n\nAvailable base sites:\n{avl_web1}\nAnd All alternate sites to droplink.co"
+    text = f"`/base_site (base_site)`\n\nCurrent base site: {site}\n\n EX: `/base_site shareus.in`\n\nAvailable base sites:\n{avl_web1}\nAnd All alternate sites to earnbylinks.com"
     if len(cmd) == 1:
         return await m.reply(
         text=text,
